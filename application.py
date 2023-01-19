9beb2725a860c97431e816b49204d760e709bf6e
Fran3000vv <fran3000g.s.w@gmail.com>
2023-01-19 20:43:47 +0100

    Test API

--- application.py
+++ application.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask,request
 
 application=Flask(__name__)
 
 @application.route('/')
 def hello_world():
+    print(request.method)
     return "<h3>Hello World</h3>"
 
 @application.route('/usuarios/<username>')
 def return_username(username):
     return username
+
+@application.post('/')
+def hello_world_post():
+    print("Hola desde el Post")
+    return "Hola"
